362f731a441e6dd4520435264b78841488610032
山内未来 <yamauchimiku@yamauchimirais-MacBook-Air.local>
2021-10-09 20:08:44 +0900

    [2 Finish]

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-alert('Hello World');
+let hello = 'Hello World'
+alert('Hello'+'World');
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
+
+// 足し算
+alert(4 + 3);
+
+// 引き算
+alert(8 - 5);
+
+// 掛け算
+alert(2 * 6);
+
+// 割り算
+alert(10 / 2);
